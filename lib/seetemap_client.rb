0bd6ebc4446935904f020365dcc933769e43af4c
Martin Catty <mcatty@synbioz.com>
2012-02-15 23:40:24 +0100

    add httparty.

--- lib/seetemap_client.rb
+++ lib/seetemap_client.rb
@@ -5,7 +5,15 @@ require 'httparty'
 
 module SeetemapClient
   class Application < Sinatra::Base
+    include HTTParty
+    base_uri 'seetemap.staging.synbioz.com'
+    format :xml
+
     get '/sitemap' do
+      auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
+      STDOUT.puts "tokens = #{auth_token}, #{site_token}"
+      sitemap = Application.get "/dashboard/websites/#{site_token}.xml", :auth_token => auth_token
+      STDOUT.puts "sitemap = #{sitemap}"
     end
 
     get '/sitemap.xml' do
