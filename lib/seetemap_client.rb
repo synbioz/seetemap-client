8f7d4a7e0ff49cd4704b2abb48a53fa5bd183c8e
Martin Catty <mcatty@synbioz.com>
2012-02-15 23:45:43 +0100

    remove xml format.

--- lib/seetemap_client.rb
+++ lib/seetemap_client.rb
@@ -7,7 +7,7 @@ module SeetemapClient
   class Application < Sinatra::Base
     include HTTParty
     base_uri 'seetemap.staging.synbioz.com'
-    format :xml
+    # format :xml
 
     get '/sitemap' do
       auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
@@ -17,6 +17,10 @@ module SeetemapClient
     end
 
     get '/sitemap.xml' do
+      auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
+      STDOUT.puts "tokens xml = #{auth_token}, #{site_token}"
+      sitemap = Application.get "/dashboard/websites/#{site_token}.xml", :auth_token => auth_token
+      STDOUT.puts "sitemap = #{sitemap}"
     end
   end
 end
