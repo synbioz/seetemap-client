72dcaf3547e025bf4fd0ea7bf1b9cf90241e7f08
Martin Catty <mcatty@synbioz.com>
2012-02-15 23:53:18 +0100

    foo

--- lib/seetemap_client.rb
+++ lib/seetemap_client.rb
@@ -10,17 +10,17 @@ module SeetemapClient
     # format :xml
 
     get '/sitemap' do
-      auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
-      STDOUT.puts "tokens = #{auth_token}, #{site_token}"
-      sitemap = Application.get "/dashboard/websites/#{site_token}.xml", :auth_token => auth_token
-      STDOUT.puts "sitemap = #{sitemap}"
+      # auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
+      # STDOUT.puts "tokens = #{auth_token}, #{site_token}"
+      # sitemap = Application.get "/dashboard/websites/#{site_token}.xml", :auth_token => auth_token
+      # STDOUT.puts "sitemap = #{sitemap}"
     end
 
-    get '/sitemap.xml' do
-      auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
-      STDOUT.puts "tokens xml = #{auth_token}, #{site_token}"
-      sitemap = Application.get "/dashboard/websites/#{site_token}.xml", :auth_token => auth_token
-      STDOUT.puts "sitemap = #{sitemap}"
-    end
+    # get '/sitemap.xml' do
+    #   auth_token, site_token = settings.seetemap_auth_token, settings.seetemap_site_token
+    #   STDOUT.puts "tokens xml = #{auth_token}, #{site_token}"
+    #   sitemap = Application.get "/dashboard/websites/#{site_token}.xml", :auth_token => auth_token
+    #   STDOUT.puts "sitemap = #{sitemap}"
+    # end
   end
 end
