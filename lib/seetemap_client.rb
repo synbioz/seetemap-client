27d139fe3665ce3f7869f045c6b7836c2042663c
Nicolas ZERMATI <nicoolas25@gmail.com>
2012-03-07 00:47:05 +0100

    Use JSON API for #2536.

--- lib/seetemap_client.rb
+++ lib/seetemap_client.rb
@@ -19,18 +19,45 @@ module SeetemapClient
       @@site_token = site_token
     end
 
+    # Call ping! before or get an empty sitemap.
+    #
+    # @return [String] sitemap content
     def self.sitemap
-      get("/fr/dashboard/websites/#{@@site_token}.xml", :query => { :auth_token => @@auth_token })
+      if self.available?
+        get("/fr/dashboard/websites/#{@@site_token}.xml", :query => { :auth_token => @@auth_token })
+      else
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"></urlset>"
+      end
     end
 
+    # Cache the response inside the Seetemap class. Must be called for each
+    # interaction with the server.
+    def self.ping!
+      @@response = get("/fr/dashboard/websites/#{@@site_token}.json", :format => :json, :query => { :auth_token => @@auth_token })
+    end
+
+    # Get the most recent timestamp that can be applyied to the audit when
+    # there is no fresher audit available. Return nil if there is no audit
+    # or no fresher audit.
+    #
+    # @param [Time] actual timestamp of the current cached sitemap
+    # @return [nil|Time] nil if there is a fresher audit available or the time to update
     def self.fresh?(time)
-      r = head("/fr/dashboard/websites/#{@@site_token}.xml", :query => { :auth_token => @@auth_token })
-      if (requested_at = r.headers["requested_at"])
-        last_modified = Time.parse(requested_at) rescue Time.now
-        time >= last_modified
-      else
-        false
+      return nil if @@response.nil?
+      case @@response["code"]
+      when 0 # no available audits or error
+        nil
+      when 1 # a finished audit is available
+        last_modified = Time.parse(@@response["audit"]["finished_at"]) rescue Time.now
+        Time.now if time >= last_modified
+      when 2 # only a running audit is available, use the previous audit
+        Time.parse(@@response["audit"]["started_at"]) rescue Time.now
       end
+    end
+
+    # @return [Boolean] true if an audit is available, false otherwise
+    def self.available?
+      @@response and @@response["code"] == 1
     end
   end
 
@@ -63,6 +90,7 @@ module SeetemapClient
     def render_sitemap
       path = "tmp/sitemap.xml"
       Seetemap.config(configuration[environment]["auth_token"], configuration[environment]["site_token"])
+      Seetemap.ping!
 
       if File.exists?(path)
         time = File.mtime(path)
@@ -70,8 +98,8 @@ module SeetemapClient
         if locally_fresh?(time)
           File.read(path)
         # api tell us it's fresh
-        elsif Seetemap.fresh?(time)
-          FileUtils.touch path
+        elsif (time_to_update = Seetemap.fresh?(time))
+          FileUtils.touch path, :mtime => time_to_update
           File.read(path)
         else
           create_sitemap_file(path)
