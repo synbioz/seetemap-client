2946cb11b301a6bc160aed2627d6a2d6b0483f73
Nicolas ZERMATI <nicoolas25@gmail.com>
2012-03-06 18:55:12 +0100

    Use the Requested-At header instead of Etag.

--- lib/seetemap_client.rb
+++ lib/seetemap_client.rb
@@ -25,8 +25,8 @@ module SeetemapClient
 
     def self.fresh?(time)
       r = head("/fr/dashboard/websites/#{@@site_token}.xml", :query => { :auth_token => @@auth_token })
-      if r.headers.key? "etag"
-        last_modified = Time.parse(r.headers["etag"]) rescue Time.now
+      if (requested_at = r.headers["requested_at"])
+        last_modified = Time.parse(requested_at) rescue Time.now
         time >= last_modified
       else
         false
